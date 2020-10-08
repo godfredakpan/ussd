7fb62ef94987c6ac86abf2190dd48d0706f38d39
Godfred <godfred.akpan@54gene.com>
2020-10-08 20:02:17 +0100

    Initial commit

--- app/Http/Controllers/USSDController.php
+++ app/Http/Controllers/USSDController.php
@@ -78,22 +78,23 @@ class USSDController extends Controller
                     //If user selected 3, exit
                     $this->ussd_stop("Thank you !");
 
+                }else if ($ussd_key[0] == '1*1') {
+                    // echo "CON ";
+                    // This is a second level response where the user selected 1 in the first instance
+                    $this->WelcomeMenu();
+
+                } else if ( $ussd_key[0] == '2*1') {
+                    // echo "CON ";
+                    // This is a second level response where the user selected 1 in the first instance
+                    $this->WelcomeMenu();
             }
             break;
 
-            case ($level == 2 && !empty($ussd_string)):
-
-                if ($ussd_key[0] == "1") {
-
+            case 2:
+                if ($ussd_key[0] == "1" and $ussd_key[1] == "1") {
                     $this->WelcomeMenu();
-
-                    $level == 1;
-
-                } else if ($ussd_key[0] == "2") {
-
+                } else if ($ussd_key[0] == "2" and $ussd_key[1] == "1") {
                     $this->WelcomeMenu();
-
-                    $level == 1;
                 }
             break;
 
