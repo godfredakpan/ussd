1f0cc7f4839d93ebd8680a3859d8f26788189f31
Godfred <godfred.akpan@54gene.com>
2020-10-08 18:04:32 +0100

    updated empty state

--- app/Http/Controllers/USSDMenu.php
+++ app/Http/Controllers/USSDMenu.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 trait USSDMenu{
 
     public function WelcomeMenu(){
-        $start  = "CON Hello ! Welcome\n";
+        $start  = "Hello ! Welcome\n";
         $start .= "1. To check your data balance\n";
         $start .= "2. To check your phone number\n";
         $start .= "3. Exit\n";
@@ -12,19 +12,19 @@ trait USSDMenu{
     }
 
     public function PhoneScreen($phone){
-        $start  = "CON My Phone Number is $phone\n";
+        $start  = "My Phone Number is $phone\n";
         $start .= "1. Previous\n";
         $this->ussd_proceed($start);
     }
 
     public function DataScreen(){
-        $start  = "CON My Data balance is 455GB\n";
+        $start  = "My Data balance is 455GB\n";
         $start .= "1. Previous\n";
         $this->ussd_proceed($start);
     }
 
     public function RewardProgram($ref_link, $phone){
-        $start  = "CON Hello ! Welcome to reward program, an sms has been sent to $phone with the referral link www.link.com/ref=$ref_link \n";
+        $start  = "Hello ! Welcome to reward program, an sms has been sent to $phone with the referral link www.link.com/ref=$ref_link \n";
         $this->ussd_stop($start);
     }
 
