bfd3b7a2dfab1797f1e5f0508d07443c9ff61a7a
Godfred <godfred.akpan@54gene.com>
2020-10-08 18:18:35 +0100

    updated empty state

--- app/Http/Controllers/USSDController.php
+++ app/Http/Controllers/USSDController.php
@@ -79,11 +79,11 @@ class USSDController extends Controller
                 }
                  else if($ussd_string_exploded[0] == "1*1") {
                     // This is a second level response where the user selected 1 in the first instance
-                    "CON".$this->WelcomeMenu();
+                    $this->ussd_stop("Thank you !");
 
-                } else if ( $ussd_string_exploded[0] == "2*1" ) {
+                } else if ( $ussd_string_exploded[0] == "2*1") {
                     // This is a second level response where the user selected 1 in the first instance
-                    "CON".$this->WelcomeMenu();
+                    $this->WelcomeMenu();
             }
             break;
 
